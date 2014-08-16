80a690072ee7bac2d32137e3177478b2f2a6450a
fadib <fahmi@fadib.net>
2014-08-16 15:01:09 -0400

    Update domain name to windowsphone.com

--- views/wpsbox-settings/page-settings.php
+++ views/wpsbox-settings/page-settings.php
@@ -2,7 +2,7 @@
 	<div id="icon-options-general" class="icon32"><br /></div>
 	<h2><?php esc_html_e( WPSBOX_NAME ); ?> Settings</h2>
 	
-	<p>In order to display the latest information from the windowsphonestore.com, please set how long you want to keep the cache data.</p>
+	<p>In order to display the latest information from the windowsphone.com, please set how long you want to keep the cache data.</p>
 	
 	<form method="post" action="options.php">
 		<?php settings_fields( $class::SETTING_SLUG ); ?>
